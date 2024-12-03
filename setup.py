3a3d19a4ae2953b97a0b3a297dc284af6d65430d
Grauly <nils.dellemann@web.de>
2024-12-03 19:46:23 +0100

    fix: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="explorhino-logger",
+    version="1.0",
+    packages=find_packages(),
+    scripts=['main.py']
+)
